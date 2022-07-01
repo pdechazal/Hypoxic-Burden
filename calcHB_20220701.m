684eb7ae64aaac19170c5dc486a4edba4a39f201
pdechazal <philip.dechazal@sydney.edu.au>
2022-07-01 10:34:56 +1000

    Add files via upload

---
+++ calcHB_20220701.m
@@ -0,0 +1,9 @@
+function HB=calcHB(SpO2,RespEvents,SleepStage,PLOTFLAG)
+%
+%Calculates hypoxic burden
+%
+%Author: Philip de Chazal
+%
+%01 July 2022
+%
+%This is a placeholder file. It will be replaced when manuscript is published
