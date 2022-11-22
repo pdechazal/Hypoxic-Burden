aecbdc263075a5f78b321130072816f57c5bea5b
pdechazal <philip.dechazal@sydney.edu.au>
2022-11-22 14:27:15 +1100

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,1 +1,6 @@
 Download all files to a Matlab accessible directory. Run HBexample from the Matlab prompt.
+
+You are free to use this code. Please cite this publication when referencing this material: 
+
+Kate Sutherland, Nadi Sadr, Yu Sun Bin, Kristina Cook, Hasthi U. Dissanayake, Peter A. Cistulli and Philip de Chazal
+"Comparative associations of oximetry patterns in Obstructive Sleep Apnea with incident cardiovascular disease", Sleep, 2022, DOI: 10.1093/sleep/zsac179
--- calcHB.m
+++ calcHB.m
@@ -1,12 +1,14 @@
 function HB=calcHB(SpO2,RespEvents,SleepStage,PLOTFLAG)
 %
 %Calculates hypoxic burden
-%21 July 2022
+%21 November 2022
 %Author: Philip de Chazal
-%Version. 1.0
-%This code was used in 
+%Version. 1.1
+%
+%You are free to use this code. Please cite this publication when referencing this material: 
 %Kate Sutherland, Nadi Sadr, Yu Sun Bin, Kristina Cook, Hasthi U. Dissanayake, Peter A. Cistulli and Philip de Chazal
-%"Comparative associations of oximetry patterns in Obstructive Sleep Apnea with incident cardiovascular disease", Sleep, 2022
+%"Comparative associations of oximetry patterns in Obstructive Sleep Apnea with incident cardiovascular disease", Sleep, 2022, DOI: 10.1093/sleep/zsac179
+%
 %
 %Inputs
 %------
